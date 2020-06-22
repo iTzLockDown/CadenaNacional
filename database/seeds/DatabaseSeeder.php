23e6ba7687400cbebb402944c5897b91d13925c5
iTzLockDown <47418895@continental.edu.pe>
2020-06-22 18:32:36 -0500

    terminado

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UserSeeder::class);
         $this->call(UsuarioSeeder::class);
+        $this->call(EmisoraSeeder::class);
     }
 }

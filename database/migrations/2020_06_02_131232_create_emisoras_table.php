e176a3096c364c032d5c0b451512e9b9430785ba
iTzLockDown <47418895@continental.edu.pe>
2020-07-02 20:32:48 -0500

    enviado terminado

--- database/migrations/2020_06_02_131232_create_emisoras_table.php
+++ database/migrations/2020_06_02_131232_create_emisoras_table.php
@@ -16,24 +16,24 @@ class CreateEmisorasTable extends Migration
         Schema::create('emisoras', function (Blueprint $table) {
             $table->id();
             $table->string('nombrecadena',30)->nullable(false);
-            $table->string('representanteLegal',50)->nullable(false);
-            $table->string('representanteComercial',50)->nullable(false);
+            $table->string('representanteLegal',150)->nullable(false);
+            $table->string('representanteComercial',150)->nullable(false);
             $table->string('frecuencia',30)->nullable(false);
             $table->string('direccion',30)->nullable(false);
             $table->string('numeroRadio',30)->nullable(false);
-            $table->string('email',30)->nullable(false);
+            $table->string('email',250)->nullable(false);
             $table->string('ruc',30)->nullable(false);
-            $table->string('descripcion',30)->nullable(false);
+            $table->string('descripcion',130)->nullable(false);
             $table->string('telefono',30)->nullable(false);
             $table->string('estacion', 20)->nullable(false);
             $table->string('estado', 1);
 
-            $table->string('nomper1',50);
-            $table->string('telper1',50);
+            $table->string('nomper1',150);
+            $table->string('telper1',150);
 
-            $table->string('nomper2',50);
-            $table->string('telper2',50);
-            $table->string('autorizacion',50);
+            $table->string('nomper2',150);
+            $table->string('telper2',150);
+            $table->string('autorizacion',150);
 
             $table->string('departamento',30)->nullable(false);
             $table->string('provincia',30)->nullable(false);
